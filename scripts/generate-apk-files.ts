8c514ed4a1820ff672b8c72a915cc0e20859b23f
currambero20 <129696472+currambero20@users.noreply.github.com>
2026-03-20 15:59:18 -0500

    Sync scripts/generate-apk-files.ts to main to trigger Render & Action fixes

--- scripts/generate-apk-files.ts
+++ scripts/generate-apk-files.ts
@@ -71,11 +71,6 @@ android {
     }
 }
 
-repositories {
-    google()
-    mavenCentral()
-}
-
 dependencies {
     implementation 'androidx.appcompat:appcompat:1.6.1'
     implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
